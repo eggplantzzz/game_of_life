03ad0928460f1306f8e7092bf3264d62cc33a388
eggplantzzz <pobol234@gmail.com>
2017-01-01 19:14:02 -0800

    Correct refactoring mistake

--- life.js
+++ life.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 
   $(".reset").on("click", start);
-  $(".next_stage").on("click", next_stage);
+  $(".next_stage").on("click", nextStage);
 
   var LENGTH = 23;
 
@@ -11,7 +11,7 @@ $(document).ready(function() {
 
     $("#board").html("");
 
-    init_board(parseInt(LENGTH));
+    initBoard(parseInt(LENGTH));
 
     /* Generates a grid                                    */
     /* Allows the user to designate cells as dead or alive */
@@ -40,7 +40,7 @@ $(document).ready(function() {
   * A 2 element denotes a cells current state and its next state
   * The first element is its current state and the second is its
   * next state.  "0" for dead and "1" for live */
-  function init_board(LENGTH) {
+  function initBoard(LENGTH) {
     var index = 0;
     var board = [];
 
@@ -58,12 +58,12 @@ $(document).ready(function() {
         index += 1;
     }
     return board;
-  } /* End init_board function */
+  } /* End initBoard function */
 
   /* Reads the html and syncs the array with it  */
   /* Sig: Takes in a length and returns an array */
-  function set_array_from_html(LENGTH) {
-    var board = init_board(LENGTH);
+  function setArrayFromHTML(LENGTH) {
+    var board = initBoard(LENGTH);
 
     var index = 0;
 
@@ -88,11 +88,39 @@ $(document).ready(function() {
   * Sets the array from the current html state, then calculates
   * the status of cells for the next stage. Then it changes the
   * cells current status to the next stage and updates the UI */
-  function next_stage() {
+  function nextStage() {
 
-    var board = set_array_from_html(LENGTH);
+    var board = setArrayFromHTML(LENGTH);
+    board = calculateAndSetNextStage(board);
     board = markBoardAndSetHTML(board);
-  } /* End next_stage function */
+  } /* End nextStage function */
+
+  /* Iterates through the board and calls count_living_neighbors to mark cells
+* for death or life */
+  function calculateAndSetNextStage(board) {
+
+    var index = 0;
+
+    while(index < board.length) {
+      var index2 = 0;
+
+      while(index2 < board.length) {
+        var living_neighbors = count_living_neighbors([index, index2], board)
+
+        if(board[index][index2][0] === "1" && (living_neighbors < 2 || living_neighbors > 3)) {
+          board[index][index2][1] = "0";
+        }else if(board[index][index2][0] === "1" && (living_neighbors === 2 || living_neighbors === 3)) {
+          board[index][index2][1] = "1";
+        }else if(board[index][index2][0] === "0" && living_neighbors === 3) {
+          board[index][index2][1] = "1";
+        }
+        index2 += 1;
+      }
+      index += 1;
+    }
+    return board
+  } /* End calculateAndSetNextStage function */
+
 
   /* Iterates through the board and calls count_living_neighbors to mark cells
   * for death or life */
@@ -103,18 +131,11 @@ $(document).ready(function() {
       var index2 = 0;
 
       while(index2 < board.length) {
-        var living_neighbors = count_living_neighbors([index, index2], board);
 
-        if(board[index][index2][0] === "1" && (living_neighbors < 2 || living_neighbors > 3)) {
-          board[index][index2][1] = "0";
+        if(board[index][index2][1] === "0") {
           board[index][index2][0] = "0";
           $("#" + index + " ." + index2).removeClass("alive").addClass("dead");
-        }else if(board[index][index2][0] === "1" && (living_neighbors === 2 || living_neighbors === 3)) {
-          board[index][index2][1] = "1";
-          board[index][index2][0] = "1";
-          $("#" + index + " ." + index2).removeClass("dead").addClass("alive");
-        }else if(board[index][index2][0] === "0" && living_neighbors === 3) {
-          board[index][index2][1] = "1";
+        }else {
           board[index][index2][0] = "1";
           $("#" + index + " ." + index2).removeClass("dead").addClass("alive");
         }
